db9613a7e657e26cd1afca6e611aa0a4c3f7eda4
SlasherOfficial <138563418+ahil15@users.noreply.github.com>
2023-12-23 15:37:07 +0600

    (#8)

--- replit.nix
+++ replit.nix
@@ -1,9 +1,18 @@
+
 { pkgs }: {
-	deps = [
-		pkgs.nano
-  pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
-	];
+    deps = [
+        pkgs.ffmpeg
+        pkgs.git
+        pkgs.nodejs-18_x
+        pkgs.nodePackages.typescript
+        pkgs.libwebp
+        pkgs.libuuid
+        pkgs.imagemagick
+        pkgs.pm2
+        pkgs.wget
+        pkgs.yarn
+    ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }
