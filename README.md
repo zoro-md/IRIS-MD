96d638a327bc6b6ab0a61fb6c4284562b0735e2e
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-03-28 13:54:14 +0600

    ɪʀɪs-ᴍᴅ

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-BOT SHUT DOWN
-WAIT FOR NEW BOT
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+<div align="center" class= "main"> 
+  <img src="https://imgur.com/D60nScQ.jpg" width="300" height="300"/>
+  <h1>ＩＲＩＳ- ＭＤ</h1>
+
+<a href="https://github.com/V-E-N-O-X"><img title="Creator" src="https://img.shields.io/badge/Creator-VENOX-red.svg?style=for-the-badge&logo=github"></a>
+<br>
+<a href="https://github.com/V-E-N-O-X?tab=followers"><img title="Followers" src="https://img.shields.io/github/followers/V-E-N-O-X?color=green&style=flat-square"></a>
+<a href="https://github.com/V-E-N-O-X/IRIS-MD/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/V-E-N-O-X/IRIS-MD?color=white&style=flat-square"></a>
+<a href="https://github.com/V-E-N-O-X/IRIS-MD/network/members"><img title="Forks" src="https://img.shields.io/github/forks/V-E-N-O-X/IRIS-MD?color=yellow&style=flat-square"></a>
+<div align="left"
+<br><br>
+  
+### SETUP
+
+1. Scan the QR and copy it
+    <br>
+<a href='https://iris-web-0cm9.onrender.com/' target="_blank"><img alt='SCAN QR' src='https://img.shields.io/badge/Scan_qr-100000?style=for-the-badge&logo=scan&logoColor=white&labelColor=black&color=black'/></a>
+
+#### DEPLOY TO HEROKU 
+
+1. Create a account.
+    <br>
+<a href='https://signup.heroku.com/' target="_blank"><img alt='Heroku' src='https://img.shields.io/badge/-Create-black?style=for-the-badge&logo=heroku&logoColor=white'/></a>
+
+2. Now Deploy
+    <br>
+<a href='https://iris-web-0cm9.onrender.com/deploy' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=heroku&logoColor=white'/></a>
+
+#### DEPLOY TO REPLIT
+
+1. Create a account.
+    <br>
+<a href='https://replit.com/signup' target="_blank"><img alt='replit' src='https://img.shields.io/badge/-Create-black?style=for-the-badge&logo=replit&logoColor=orange'/></a>
+
+2. Now Deploy
+    <br>
+<a href='https://replit.com/github/V-E-N-O-X/IRIS-MD' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=replit&logoColor=orange'/></a>
+
+#### DEPLOY TO RAILWAY
+
+1. Create a account.
+    <br>
+<a href='https://railway.app/login' target="_blank"><img alt='railway' src='https://img.shields.io/badge/-Create-black?style=for-the-badge&logo=railway&logoColor=white'/></a>
+
+2. Now Deploy
+    <br>
+<a href='https://railway.app/template/12FD9C?referralCode=Wbbu1p' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=railway&logoColor=white'/></a>
+<br><br>
+
+### Support Me
+
+<a href="https://whatsapp.com/channel/0029VaHt1710AgWB1B0Lkg0Q"><img alt="WhatsApp" src="https://img.shields.io/badge/-Whatsapp%20Channel-white?style=for-the-badge&logo=whatsapp&logoColor=black"/></a>
+<br><br>
+
+  ### THANKS TO
+- [ABRAHAM ❤️](https://github.com/abrahamdw882) <br>
+- [X-Electra](https://github.com/X-Electra) for [X-Asena](https://github.com/X-Electra/X-Asena)
+<br><br>
+
+### 📮```S&K```
+
+- *I will only Assist You in Bot Deployment*
+- *Do not revamp my modules/plugins*
+- *This Bot is not 100% secure,So keep this in mind*
+- ***Regards [Venox-Senpai](https://github.com/V-E-N-O-X)***
+
+  
+
---
+++ iris_plugins/serialize.js
@@ -0,0 +1,48 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════// 
+//                                                                                                      //
+//                                     LIGHT-WEIGHT WHATSAPP BOT                                        //
+//                                                                                                      //
+//                                             𝚅.1.2.7                                                  // 
+//                                                                                                      //
+//                          ██╗ ██████╗ ██╗███████╗    ███╗   ███╗ ██████╗                              //
+//                          ██║ ██╔══██╗██║██╔════╝    ████╗ ████║ ██╔══██╗                             //
+//                          ██║ ██████╔╝██║███████╗    ██╔████╔██║ ██║  ██║                             //
+//                          ██║ ██╔══██╗██║╚════██║    ██║╚██╔╝██║ ██║  ██║                             //
+//                          ██║ ██║  ██║██║███████║    ██║ ╚═╝ ██║ ██████╔╝                             //
+//                          ╚═╝ ╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝                              //
+//                                                                                                      //
+//                                          BY: VENOX-SENPAI                                            //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+/**
+
+* @project_name : IRIS-MD
+* @Developer : Venox-Senpai
+* @Version : v.1.2.7
+* @license : APACHE-2.0
+
+THIS PROJECT IS MADE UNDER THE CIRCUMSTANCES OF "APACHE-2.0"
+MEANS ANY KIND OF (MISLEADING, RE-UPLOADING, RENG) WILL LEAD YOU
+TO A DMCA REPORT FROM THE OFFICIAL GITHUB, WHICH RESULT IN 
+REMOVING UR COPIED/RE-UPLOADED PROJECT FROM THERE, IN SOME
+CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
+
+│• @C_holder : Venox-Senpai
+
+**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _0x271a(_0x23f2a6,_0x13a097){const _0x176a96=_0x2f9a();return _0x271a=function(_0x53209b,_0x5542b9){_0x53209b=_0x53209b-0x9a;let _0x2f9a50=_0x176a96[_0x53209b];return _0x2f9a50;},_0x271a(_0x23f2a6,_0x13a097);}function _0x2f9a(){const _0x156823=['2ZzWMZh','https://api-viper-x.koyeb.app/api/song?name=','45GzZYvb','key','video','sender','title','Audio','user','Error\x20fetching\x20data.','3301281gxZUGC','downloadUrl','jid','3789961SeZpKG','get','Video','No\x20URL\x20found\x20in\x20the\x20text.','match','167252sqTEnB','11896932fWNTEX','name','apply','\x0a_Url:\x20','Youtube\x20Downloader','1083035oKBUGo','32FFaRrF','Searching...','469446ePJtkE','url','8569810ZlkZBi','split','quoted','https://api-viper-x.koyeb.app/api/insta?url=','text','33ETcmck','sendMsg','buffer','(((.+)+)+)+$','data','toString','audio/mpeg','sendMessage','search','93qBVwPC','includes','thumbnail','type','Select\x20Type','Error\x20fetching\x20data:','trim'];_0x2f9a=function(){return _0x156823;};return _0x2f9a();}(function(_0xa56015,_0x585f6c){const _0x12a8af=_0x271a,_0x1abf32=_0xa56015();while(!![]){try{const _0xa7738b=-parseInt(_0x12a8af(0xb7))/0x1*(-parseInt(_0x12a8af(0x9f))/0x2)+-parseInt(_0x12a8af(0xca))/0x3*(parseInt(_0x12a8af(0xb1))/0x4)+-parseInt(_0x12a8af(0xa1))/0x5*(parseInt(_0x12a8af(0xba))/0x6)+-parseInt(_0x12a8af(0xac))/0x7+parseInt(_0x12a8af(0xb8))/0x8*(-parseInt(_0x12a8af(0xa9))/0x9)+parseInt(_0x12a8af(0xbc))/0xa+parseInt(_0x12a8af(0xc1))/0xb*(parseInt(_0x12a8af(0xb2))/0xc);if(_0xa7738b===_0x585f6c)break;else _0x1abf32['push'](_0x1abf32['shift']());}catch(_0x20ccdf){_0x1abf32['push'](_0x1abf32['shift']());}}}(_0x2f9a,0xdd002));const _0x5542b9=(function(){let _0x40f7de=!![];return function(_0x590c07,_0x59caa5){const _0x39f614=_0x40f7de?function(){const _0x4a9b97=_0x271a;if(_0x59caa5){const _0x1b953f=_0x59caa5[_0x4a9b97(0xb4)](_0x590c07,arguments);return _0x59caa5=null,_0x1b953f;}}:function(){};return _0x40f7de=![],_0x39f614;};}()),_0x53209b=_0x5542b9(this,function(){const _0x12c01e=_0x271a;return _0x53209b[_0x12c01e(0xc6)]()['search'](_0x12c01e(0xc4))[_0x12c01e(0xc6)]()['constructor'](_0x53209b)[_0x12c01e(0xc9)](_0x12c01e(0xc4));});_0x53209b(),iris({'on':'text','fromMe':isPublic},async({m:_0x28a4f4,client:_0x3a3bdd,args:_0x25d6c6})=>{const _0x3988a7=_0x271a;if(!_0x28a4f4[_0x3988a7(0xbe)])return;try{if(_0x3a3bdd[_0x3988a7(0xa7)]['id']['split'](':')[0x0]===_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xa4)][_0x3988a7(0xbd)]('@')[0x0]&&parseInt(_0x28a4f4[_0x3988a7(0xc0)])){const _0x1d1773=parseInt(_0x28a4f4[_0x3988a7(0xc0)]);if(_0x1d1773>=0x1&&_0x1d1773<=0xb){if(_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xc0)][_0x3988a7(0xcb)](_0x3988a7(0xb6))){const _0x11d282=_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xc0)][_0x3988a7(0xbd)]('\x0a'),_0xae52a8=_0x11d282[_0x1d1773+0x1][_0x3988a7(0xbd)]('\x20.')[0x1][_0x3988a7(0x9e)](),_0x35bb98={'text':_0x3988a7(0xb9)};let _0x262581=await _0x3a3bdd[_0x3988a7(0xc8)](_0x28a4f4[_0x3988a7(0xab)],_0x35bb98,{'quoted':_0x28a4f4});const _0x55a3c9=await axios[_0x3988a7(0xad)](_0x3988a7(0xa0)+_0xae52a8);let _0x4fbe5e=await _0x55a3c9[_0x3988a7(0xc5)];const _0x245fcf={'text':'Select\x20Type\x0a\x0a_Title:\x20'+_0x4fbe5e[_0x3988a7(0xc5)][_0x3988a7(0xa5)]+_0x3988a7(0xb5)+_0x4fbe5e[_0x3988a7(0xc5)][_0x3988a7(0xbb)]+'\x0a\x0a_1\x20.Audio_\x0a_2\x20.Video_\x0a\x0a_Reply\x20with\x20Number_','edit':_0x262581[_0x3988a7(0xa2)]};await _0x3a3bdd[_0x3988a7(0xc8)](_0x28a4f4[_0x3988a7(0xab)],_0x245fcf,{'quoted':_0x28a4f4});}if(_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xc0)][_0x3988a7(0xcb)]('Total\x20Stories')){const _0x391ff5=_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xc0)][_0x3988a7(0xb0)](/Url : (.+)/);if(_0x391ff5&&_0x391ff5[0x1]){const _0x59bbed=_0x391ff5[0x1][_0x3988a7(0x9e)]();try{const _0xf1d583=await axios[_0x3988a7(0xad)](_0x3988a7(0xbf)+_0x59bbed),_0x42c312=_0xf1d583[_0x3988a7(0xc5)];_0x28a4f4['sendMsg'](_0x28a4f4[_0x3988a7(0xab)],_0x42c312[_0x3988a7(0xc5)][_0x1d1773-0x1][_0x3988a7(0xbb)],{},_0x42c312[_0x3988a7(0xc5)][_0x1d1773-0x1][_0x3988a7(0x9b)]);}catch(_0x125fd7){console['error'](_0x3988a7(0x9d),_0x125fd7),_0x28a4f4['sendMsg'](_0x28a4f4[_0x3988a7(0xab)],_0x3988a7(0xa8),{'quoted':_0x28a4f4});}}else _0x28a4f4[_0x3988a7(0xc2)](_0x28a4f4['jid'],_0x3988a7(0xaf),{'quoted':_0x28a4f4});}if(_0x28a4f4['quoted']['text'][_0x3988a7(0xcb)](_0x3988a7(0x9c))){const _0xf462e2=_0x28a4f4[_0x3988a7(0xbe)][_0x3988a7(0xc0)]['split']('\x0a'),_0x31e907=_0xf462e2[_0x1d1773+0x4][_0x3988a7(0x9e)]()['replace'](/^\d+\s*\./,''),_0x25d5c5=_0x28a4f4['quoted'][_0x3988a7(0xc0)][_0x3988a7(0xb0)](/Url: (https?:\/\/[^\s]+)/),_0x26dd5e=_0x25d5c5&&_0x25d5c5[0x1],_0x588894={'text':'Downloading...'};let _0x4c149b=await _0x3a3bdd['sendMessage'](_0x28a4f4[_0x3988a7(0xab)],_0x588894,{'quoted':_0x28a4f4});const _0x8e6101=await axios[_0x3988a7(0xad)](_0x3988a7(0xa0)+_0x26dd5e);let _0x509949=await _0x8e6101[_0x3988a7(0xc5)];if(_0x31e907[_0x3988a7(0xcb)](_0x3988a7(0xa6))){let _0xfd7b7b=await(await fetch(''+_0x509949[_0x3988a7(0xc5)][_0x3988a7(0x9a)]))[_0x3988a7(0xc3)](),_0x1d6afb=await(await fetch(''+_0x509949[_0x3988a7(0xc5)][_0x3988a7(0xaa)]))[_0x3988a7(0xc3)]();const _0x34b883={'text':'Downloading\x20:\x20'+_0x509949[_0x3988a7(0xc5)][_0x3988a7(0xa5)]+'','edit':_0x4c149b['key']};await _0x3a3bdd[_0x3988a7(0xc8)](_0x28a4f4['jid'],_0x34b883,{'quoted':_0x28a4f4});const _0xd5de60={'title':_0x509949[_0x3988a7(0xc5)][_0x3988a7(0xa5)],'artist':_0x509949[_0x3988a7(0xc5)]['channel'][_0x3988a7(0xb3)]},_0x3da3e5=await AddMp3Meta(_0x1d6afb,_0xfd7b7b,_0xd5de60),_0xb6a847={'audio':_0x3da3e5,'mimetype':_0x3988a7(0xc7)};return _0x3a3bdd[_0x3988a7(0xc8)](_0x28a4f4[_0x3988a7(0xab)],_0xb6a847,{'quoted':_0x28a4f4});}if(_0x31e907[_0x3988a7(0xcb)](_0x3988a7(0xae))){let _0x438c15=await(await fetch('https://api-viper-x.koyeb.app/api/ytdl?video360p='+_0x509949[_0x3988a7(0xc5)][_0x3988a7(0xbb)]))[_0x3988a7(0xc3)]();_0x28a4f4[_0x3988a7(0xc2)](_0x28a4f4[_0x3988a7(0xab)],_0x438c15,{},_0x3988a7(0xa3));}}}else return;}}catch(_0x434f67){console['error'](_0x434f67);}});
