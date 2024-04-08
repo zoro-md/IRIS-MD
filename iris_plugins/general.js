b62208e458fdc51955d5db13c4eb6435c05c7597
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-04-08 20:58:39 +0600

    ꜰɪxᴇᴅ

--- iris_plugins/general.js
+++ iris_plugins/general.js
@@ -45,4 +45,122 @@ CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
 
 
 
-var _0x5f3869=_0x4df4;(function(_0x4739c5,_0x104b9a){var _0x4ad0d1=_0x4df4,_0x2d22e4=_0x4739c5();while(!![]){try{var _0x30ce93=parseInt(_0x4ad0d1(0x20c))/0x1+-parseInt(_0x4ad0d1(0x213))/0x2*(parseInt(_0x4ad0d1(0x1fe))/0x3)+-parseInt(_0x4ad0d1(0x1fb))/0x4+-parseInt(_0x4ad0d1(0x1f1))/0x5+-parseInt(_0x4ad0d1(0x1e2))/0x6*(-parseInt(_0x4ad0d1(0x201))/0x7)+parseInt(_0x4ad0d1(0x1f8))/0x8*(parseInt(_0x4ad0d1(0x1e8))/0x9)+parseInt(_0x4ad0d1(0x20b))/0xa*(parseInt(_0x4ad0d1(0x1e5))/0xb);if(_0x30ce93===_0x104b9a)break;else _0x2d22e4['push'](_0x2d22e4['shift']());}catch(_0x171eaa){_0x2d22e4['push'](_0x2d22e4['shift']());}}}(_0x4fcd,0x6391d));var _0x4536aa=(function(){var _0x3e0e10=!![];return function(_0x5d09a8,_0x20f9ee){var _0x2c2069=_0x3e0e10?function(){var _0x134c8e=_0x4df4;if(_0x20f9ee){var _0x3db614=_0x20f9ee[_0x134c8e(0x1e4)](_0x5d09a8,arguments);return _0x20f9ee=null,_0x3db614;}}:function(){};return _0x3e0e10=![],_0x2c2069;};}()),_0x1e8e16=_0x4536aa(this,function(){var _0x50bd0d=_0x4df4;return _0x1e8e16[_0x50bd0d(0x1f4)]()[_0x50bd0d(0x1f2)]('(((.+)+)+)+$')[_0x50bd0d(0x1f4)]()[_0x50bd0d(0x1f6)](_0x1e8e16)[_0x50bd0d(0x1f2)](_0x50bd0d(0x1ef));});function _0x4df4(_0x3283bb,_0x12ab07){var _0x4f420c=_0x4fcd();return _0x4df4=function(_0x1e8e16,_0x4536aa){_0x1e8e16=_0x1e8e16-0x1e1;var _0x4fcdd6=_0x4f420c[_0x1e8e16];return _0x4fcdd6;},_0x4df4(_0x3283bb,_0x12ab07);}_0x1e8e16();const {iris,commands,isPublic}=require(_0x5f3869(0x1e6)),font=require(_0x5f3869(0x202));iris({'name':_0x5f3869(0x203),'fromMe':isPublic,'desc':_0x5f3869(0x214),'category':_0x5f3869(0x1e3)},async({m:_0x5eb09f,client:_0x41c136,args:_0x19bbc7})=>{var _0x5aef7c=_0x5f3869;if(!await _0x5eb09f[_0x5aef7c(0x20e)](_0x41c136['user']['id']))return _0x5eb09f[_0x5aef7c(0x1fc)](_0x5aef7c(0x206));if(!await _0x5eb09f[_0x5aef7c(0x20e)](_0x5eb09f[_0x5aef7c(0x1ed)]))return _0x5eb09f[_0x5aef7c(0x1fc)]('_Only\x20for\x20authorized\x20administrators._');let _0x534eb8=await _0x41c136[_0x5aef7c(0x1ee)](_0x5eb09f[_0x5aef7c(0x1eb)]);return _0x5eb09f['reply'](_0x5aef7c(0x1e1)+_0x534eb8);}),iris({'name':'mute','fromMe':isPublic,'desc':'Mutes\x20the\x20group.','category':_0x5f3869(0x1e3)},async({m:_0x4fc109,client:_0x54a85c,args:_0x2db581})=>{var _0x311be6=_0x5f3869;if(!await _0x4fc109[_0x311be6(0x20e)](_0x54a85c['user']['id']))return _0x4fc109[_0x311be6(0x1fc)]('_Admin\x20access\x20not\x20conferred._');if(!await _0x4fc109[_0x311be6(0x20e)](_0x4fc109[_0x311be6(0x1ed)]))return _0x4fc109[_0x311be6(0x1fc)]('_Only\x20for\x20authorized\x20administrators._');return await _0x54a85c[_0x311be6(0x212)](_0x4fc109['jid'],_0x311be6(0x20f)),await _0x4fc109['reply']('_Messages\x20restricted\x20to\x20group\x20admins._');}),iris({'name':'unmute','fromMe':isPublic,'desc':'Unmutes\x20the\x20group','category':'group'},async({m:_0xb294e7,client:_0x31b948,args:_0x4f0160})=>{var _0x2bd41f=_0x5f3869;if(!await _0xb294e7['isAdmin'](_0x31b948[_0x2bd41f(0x1f3)]['id']))return _0xb294e7['reply']('_Admin\x20access\x20not\x20conferred._');if(!await _0xb294e7[_0x2bd41f(0x20e)](_0xb294e7[_0x2bd41f(0x1ed)]))return _0xb294e7[_0x2bd41f(0x1fc)](_0x2bd41f(0x208));return await _0x31b948[_0x2bd41f(0x212)](_0xb294e7['jid'],_0x2bd41f(0x1ea)),await _0xb294e7['reply'](_0x2bd41f(0x205));}),iris({'name':_0x5f3869(0x211),'fromMe':isPublic,'desc':_0x5f3869(0x1f7),'category':'group'},async({m:_0xabdf22,client:_0x367003,args:_0x46cfd5})=>{var _0x335129=_0x5f3869;try{if(!await _0xabdf22[_0x335129(0x20e)](_0x367003[_0x335129(0x1f3)]['id']))return _0xabdf22[_0x335129(0x1fc)](_0x335129(0x206));if(!await _0xabdf22[_0x335129(0x20e)](_0xabdf22[_0x335129(0x1ed)]))return _0xabdf22[_0x335129(0x1fc)](_0x335129(0x208));if(!(_0x46cfd5||_0xabdf22[_0x335129(0x210)]))return _0xabdf22[_0x335129(0x1fc)]('_Mention\x20a\x20user._');if(_0x46cfd5)var _0x162079=_0x46cfd5[_0x335129(0x1ff)]('@','')+_0x335129(0x1fa);else{if(_0xabdf22[_0x335129(0x210)]['sender'])var _0x162079=_0xabdf22[_0x335129(0x210)][_0x335129(0x1ed)];else{if(_0xabdf22['mentionedJid'])var _0x162079=_0x46cfd5+'@s.whatsapp.net';}}if(await _0xabdf22[_0x335129(0x20e)](_0x162079)===!![])return _0xabdf22[_0x335129(0x1fc)](_0x335129(0x1f9));await _0x367003[_0x335129(0x216)](_0xabdf22['jid'],[_0x162079],'promote'),_0xabdf22[_0x335129(0x20d)](_0xabdf22[_0x335129(0x1eb)],'_@'+_0x162079[_0x335129(0x215)]('@')[0x0]+_0x335129(0x1e9),{'mentions':[_0x162079],'quoted':_0xabdf22});}catch(_0x16ccc8){console['log'](_0x16ccc8);}}),iris({'name':_0x5f3869(0x1f0),'fromMe':isPublic,'desc':_0x5f3869(0x204),'category':'group'},async({m:_0x34295b,client:_0x28415a,args:_0x1f37db})=>{var _0x5e213e=_0x5f3869;try{if(!await _0x34295b[_0x5e213e(0x20e)](_0x28415a[_0x5e213e(0x1f3)]['id']))return _0x34295b[_0x5e213e(0x1fc)](_0x5e213e(0x206));if(!await _0x34295b[_0x5e213e(0x20e)](_0x34295b['sender']))return _0x34295b['reply'](_0x5e213e(0x208));if(!(_0x1f37db||_0x34295b[_0x5e213e(0x210)]))return _0x34295b[_0x5e213e(0x1fc)]('_Mention\x20a\x20user._');if(_0x1f37db)var _0x43c2d0=_0x1f37db[_0x5e213e(0x1ff)]('@','')+_0x5e213e(0x1fa);else{if(_0x34295b[_0x5e213e(0x210)]['sender'])var _0x43c2d0=_0x34295b[_0x5e213e(0x210)][_0x5e213e(0x1ed)];else{if(_0x34295b[_0x5e213e(0x200)])var _0x43c2d0=_0x1f37db+_0x5e213e(0x1fa);}}if(!await _0x34295b[_0x5e213e(0x20e)](_0x43c2d0))return _0x34295b[_0x5e213e(0x1fc)](_0x5e213e(0x209));await _0x28415a[_0x5e213e(0x216)](_0x34295b[_0x5e213e(0x1eb)],[_0x43c2d0],_0x5e213e(0x1f0)),_0x34295b[_0x5e213e(0x20d)](_0x34295b['jid'],'_@'+_0x43c2d0[_0x5e213e(0x215)]('@')[0x0]+_0x5e213e(0x20a),{'mentions':[_0x43c2d0],'quoted':_0x34295b});}catch(_0x3c1d2a){console[_0x5e213e(0x1f5)](_0x3c1d2a);}}),iris({'name':_0x5f3869(0x207),'fromMe':!![],'category':_0x5f3869(0x1e3),'desc':''},async({m:_0x205a3e,client:_0x14afa4})=>{var _0x336c28=_0x5f3869;if(!_0x205a3e[_0x336c28(0x1ec)])return await _0x205a3e[_0x336c28(0x1fc)](_0x336c28(0x1fd));await _0x14afa4[_0x336c28(0x1e7)](_0x205a3e[_0x336c28(0x1eb)]);});function _0x4fcd(){var _0x4da6a7=['apply','121lUFvLF','../lib/commands.js','groupLeave','1052451HiSYAR','\x20promoted\x20to\x20admin\x20role._','not_announcement','jid','isGroup','sender','groupInviteCode','(((.+)+)+)+$','demote','3764610NmNlbO','search','user','toString','log','constructor','Promotes\x20a\x20user\x20to\x20admin','48PpLiwo','_The\x20user\x20is\x20currently\x20in\x20an\x20admin\x20role._','@s.whatsapp.net','486624Teljit','reply','*_This\x20command\x20can\x20only\x20be\x20used\x20in\x20group!_*','4491UjbIzU','replace','mentionedJid','236943zKqOmJ','@viper-x/fancytext','invite','Demotes\x20a\x20user\x20from\x20admin','_Messages\x20Unrestricted._','_Admin\x20access\x20not\x20conferred._','left','_Only\x20for\x20authorized\x20administrators._','_Admin\x20access\x20not\x20extended\x20to\x20the\x20user._','\x20demoted\x20from\x20admin\x20role!_','216110erBNzr','347050xMhnMV','sendMsg','isAdmin','announcement','quoted','promote','groupSettingUpdate','548kXelBc','Share\x27s\x20Group\x20invitation\x20link','split','groupParticipantsUpdate','https://chat.whatsapp.com/','72KwjMIG','group'];_0x4fcd=function(){return _0x4da6a7;};return _0x4fcd();}
+
+
+// Type 1: Group Management Commands
+//───────────────────────────────────
+
+iris({
+    name: "mute",
+    fromMe: isPublic,
+    desc: "Mutes the group.",
+    category: "group"
+}, async ({ m, client, args }) => {
+    if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+    await client.groupSettingUpdate(m.jid, 'announcement');
+    return await m.reply("_Group messages restricted to admins._");
+});
+
+iris({
+    name: "unmute",
+    fromMe: isPublic,
+    desc: "Unmutes the group",
+    category: "group"
+}, async ({ m, client, args }) => {
+    if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+    await client.groupSettingUpdate(m.jid, 'not_announcement');
+    return await m.reply("_Group messages unrestricted._");
+});
+
+iris({
+    name: "promote",
+    fromMe: isPublic,
+    desc: "Promotes a user to admin",
+    category: "group"
+}, async ({ m, client, args }) => {
+    try {
+        if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+        if (!(args || m.quoted)) return m.reply("_Mention a user to promote._");
+
+        let user = args ? args.replace("@", "") + '@s.whatsapp.net' : m.quoted.sender;
+
+        if (!m.isAdmin(user)) return m.reply("_The user is already an admin._");
+
+        await client.groupParticipantsUpdate(m.jid, [user], "promote");
+        m.sendMsg(m.jid , `_@${user.split("@")[0]} has been promoted to admin._`, { mentions : [user] , quoted : m})
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+iris({
+    name: "demote",
+    fromMe: isPublic,
+    desc: "Demotes a user from admin",
+    category: "group"
+}, async ({ m, client, args }) => {
+    try {
+        if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+        if (!(args || m.quoted)) return m.reply("_Mention a user to demote._");
+
+        let user = args ? args.replace("@", "") + '@s.whatsapp.net' : m.quoted.sender;
+
+        if (!m.isAdmin(user)) return m.reply("_The user is not an admin._");
+
+        await client.groupParticipantsUpdate(m.jid, [user], "demote");
+        m.sendMsg(m.jid , `_@${user.split("@")[0]} has been demoted from admin role._`, { mentions : [user] , quoted : m })
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// Type 2: Group Actions
+//────────────────────────
+
+iris({
+    name : "left",
+    fromMe: true,
+    category : "group",
+    desc: "Leave the group.",
+}, async ({ m, client }) => {
+    if (!m.isGroup) {
+        return await m.reply("*_This command can only be used in a group!_*");
+    }
+    await client.groupLeave(m.jid);
+});
+
+iris({
+    name : "invite",
+    fromMe: true,
+    category : "group",
+    desc: "Generate group invite link.",
+}, async ({ m, client }) => {
+    if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+    const response = await client.groupInviteCode(m.jid);
+    await m.reply(`_Group invite link: https://chat.whatsapp.com/${response}_`);
+});
+
+iris({
+    name : "revoke",
+    fromMe: true,
+    category : "group",
+    desc: "Revoke group invite link.",
+}, async ({ m, client }) => {
+    if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+    await client.groupRevokeInvite(m.jid);
+    await m.reply("_The group invite link has been revoked._");
+});
+
+iris({
+    name : "join",
+    fromMe: true,
+    category : "group",
+    desc: "Join a group using invite link.",
+}, async ({ m, client, args }) => {
+    if (!m.isAdmin) return m.reply("_You do not have administrative privileges._");
+    const rgx = /^(https?:\/\/)?chat\.whatsapp\.com\/(?:invite\/)?([a-zA-Z0-9_-]{22})$/;
+    if (!args || !rgx.test(args)) return await m.reply("_Please provide a valid group invite link._");
+    const res = await client.groupAcceptInvite(args.split("/")[3]);
+    if (!res) return await m.reply("_Invalid group link!_");
+    return await m.reply("_Successfully joined the group!_");
+});
