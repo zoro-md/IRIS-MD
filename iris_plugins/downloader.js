cf03aba0be40ea12a07726fc252cba7ba7c3fde5
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-04-08 20:27:29 +0600

    sᴜᴍ ᴄʜᴀɴɢᴇs ᴀɴᴅ ɴᴇᴡ sᴛᴜꜰꜰ 🍀

--- iris_plugins/downloader.js
+++ iris_plugins/downloader.js
@@ -45,4 +45,196 @@ CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
 
 
 
-const _0x10aa02=_0x5d1f;(function(_0x479afc,_0x453e48){const _0x302dd9=_0x5d1f,_0x59b2e3=_0x479afc();while(!![]){try{const _0x52d3fd=parseInt(_0x302dd9(0x139))/0x1*(-parseInt(_0x302dd9(0x12f))/0x2)+parseInt(_0x302dd9(0x150))/0x3*(parseInt(_0x302dd9(0x124))/0x4)+parseInt(_0x302dd9(0x147))/0x5+-parseInt(_0x302dd9(0x141))/0x6*(parseInt(_0x302dd9(0x142))/0x7)+parseInt(_0x302dd9(0x15c))/0x8+parseInt(_0x302dd9(0x12c))/0x9*(-parseInt(_0x302dd9(0x130))/0xa)+-parseInt(_0x302dd9(0x15a))/0xb*(parseInt(_0x302dd9(0x129))/0xc);if(_0x52d3fd===_0x453e48)break;else _0x59b2e3['push'](_0x59b2e3['shift']());}catch(_0xc60111){_0x59b2e3['push'](_0x59b2e3['shift']());}}}(_0x592f,0x2d743));const _0x2e470a=(function(){let _0x9c7f34=!![];return function(_0x5d1c0c,_0x3f3cb8){const _0x1ca05b=_0x9c7f34?function(){const _0x47e7b9=_0x5d1f;if(_0x3f3cb8){const _0x346959=_0x3f3cb8[_0x47e7b9(0x131)](_0x5d1c0c,arguments);return _0x3f3cb8=null,_0x346959;}}:function(){};return _0x9c7f34=![],_0x1ca05b;};}()),_0x4ac990=_0x2e470a(this,function(){const _0xe6bedc=_0x5d1f;return _0x4ac990[_0xe6bedc(0x145)]()['search']('(((.+)+)+)+$')['toString']()[_0xe6bedc(0x14c)](_0x4ac990)[_0xe6bedc(0x123)]('(((.+)+)+)+$');});_0x4ac990();function _0x5d1f(_0x44e423,_0x31c45a){const _0x27c518=_0x592f();return _0x5d1f=function(_0x4ac990,_0x2e470a){_0x4ac990=_0x4ac990-0x121;let _0x592ff5=_0x27c518[_0x4ac990];return _0x592ff5;},_0x5d1f(_0x44e423,_0x31c45a);}function _0x592f(){const _0x2ad1a0=['36KztJnb','173593EaIejO','https://api.maher-zubair.tech/download/mediafire?url=','_Enter\x20Query,Number_','toString','jid','1065520FRyXXR','split','https://api.maher-zubair.tech/download/gdrive?url=','log','axios','constructor','application/zip','_Downloading..._','story','2655izZVxl','xvdl','fileName','push','image','sendMessage','text','downloadUrl','insta','_Just\x20a\x20moment..._','99bjNvwZ','sendMsg','2657576lIOzSk','get','_Error_','name','\x20.\x20','g-i-s','gdrive','search','484nKEzWl','json','length','\x20images\x20for\x20','reply','54900XiNlBo','_Enter\x20Link/Reply\x20to\x20a\x20link_','application/x-zip-compressed','1622601WGTwuj','https://api-aswin-sparky.koyeb.app/api/downloader/xdl?url=','mediafire','2ywIAbY','10uqfnsQ','apply','ehehehe','Instagram\x20downloader','Instagram\x20story\x20downloader','key','_Enter\x20Link_','quoted','https://api.maher-zubair.tech/download/instagram?url=','95949epRoTb','node-fetch','_Downloading\x20','downloader','Google\x20Image\x20search','link','data','Total\x20Stories\x0aUrl\x20:\x20'];_0x592f=function(){return _0x2ad1a0;};return _0x592f();}const {iris,commands,isPublic}=require('../lib/commands.js');let gis=require(_0x10aa02(0x121));const axios=require(_0x10aa02(0x14b)),fetch=require(_0x10aa02(0x13a));iris({'name':_0x10aa02(0x158),'fromMe':isPublic,'desc':_0x10aa02(0x133),'category':_0x10aa02(0x13c)},async({m:_0x28c554,client:_0x408d92,args:_0x1f899e})=>{const _0x313ede=_0x10aa02;_0x1f899e=_0x1f899e||_0x28c554[_0x313ede(0x137)]?.[_0x313ede(0x156)];if(!_0x1f899e)return await _0x28c554[_0x313ede(0x128)](_0x313ede(0x136));let _0x2b35ea=await _0x408d92[_0x313ede(0x155)](_0x28c554[_0x313ede(0x146)],{'text':_0x313ede(0x14e)},{'quoted':_0x28c554});try{const _0x2429dc=await axios[_0x313ede(0x15d)](_0x313ede(0x138)+_0x1f899e);let _0x51de68=await _0x2429dc[_0x313ede(0x13f)];for(let _0x1dbba2 of _0x51de68[_0x313ede(0x13f)]){await _0x28c554['sendMsg'](_0x28c554[_0x313ede(0x146)],_0x1dbba2['url'],{'quoted':_0x28c554},_0x1dbba2['type']);}}catch(_0x57ba36){_0x408d92[_0x313ede(0x155)](_0x28c554[_0x313ede(0x146)],{'text':_0x313ede(0x15e),'edit':_0x2b35ea[_0x313ede(0x135)]});}}),iris({'name':_0x10aa02(0x14f),'fromMe':isPublic,'desc':_0x10aa02(0x134),'category':'downloader'},async({m:_0x47b9d4,client:_0x1b8a7f,args:_0x2afee7})=>{const _0x29300f=_0x10aa02;_0x2afee7=_0x2afee7||_0x47b9d4[_0x29300f(0x137)]?.[_0x29300f(0x156)];if(!_0x2afee7)return await _0x47b9d4['reply'](_0x29300f(0x136));let _0x1a2e0e=await _0x1b8a7f['sendMessage'](_0x47b9d4[_0x29300f(0x146)],{'text':_0x29300f(0x159)},{'quoted':_0x47b9d4}),_0x2a80f4=_0x2afee7,_0x3b60d8=await axios[_0x29300f(0x15d)]('https://api-aswin-sparky.koyeb.app/api/downloader/story?url='+_0x2a80f4),_0x2b9ec1=await _0x3b60d8[_0x29300f(0x13f)],_0x25a564=_0x2b9ec1[_0x29300f(0x13f)][0x0],_0x14fbc6=_0x29300f(0x140)+_0x2a80f4+'\x0a\x0a';for(let _0x459563=0x1;_0x459563<_0x2b9ec1[_0x29300f(0x13f)]['length']+0x1;_0x459563++){_0x14fbc6+='_'+_0x459563+_0x29300f(0x160)+_0x459563+'/'+_0x2b9ec1[_0x29300f(0x13f)]['length']+'\x20-\x20'+_0x2b9ec1[_0x29300f(0x13f)][_0x459563-0x1]['type']+'_\x0a';}_0x14fbc6+='\x0aReply\x20with\x20Number',_0x47b9d4[_0x29300f(0x15b)](_0x47b9d4[_0x29300f(0x146)],_0x14fbc6,{'edit':_0x1a2e0e[_0x29300f(0x135)]});}),iris({'name':_0x10aa02(0x154),'fromMe':isPublic,'desc':_0x10aa02(0x13d),'category':_0x10aa02(0x13c)},async({m:_0x4db153,client:_0x504be7,args:_0x10f18f})=>{const _0x723d9e=_0x10aa02;try{async function _0xdb78d2(_0x17de6e,_0x498efb=0x5){let _0x5a2615=[];return new Promise((_0xaeb9f3,_0x29e4dd)=>{gis(_0x17de6e,async(_0x5b95df,_0x3b7495)=>{const _0x2b2311=_0x5d1f;for(var _0x3d359c=0x0;_0x3d359c<(_0x3b7495[_0x2b2311(0x126)]<_0x498efb?_0x3b7495[_0x2b2311(0x126)]:_0x498efb);_0x3d359c++){_0x5a2615[_0x2b2311(0x153)](_0x3b7495[_0x3d359c]['url']),_0xaeb9f3(_0x5a2615);}});});}if(!_0x10f18f)return await _0x4db153['reply'](_0x723d9e(0x144));let [_0x97577c,_0x493aa6]=_0x10f18f[_0x723d9e(0x148)](','),_0x1f6ee5=await _0xdb78d2(_0x97577c,_0x493aa6);await _0x4db153['reply'](_0x723d9e(0x13b)+(_0x493aa6||0x5)+_0x723d9e(0x127)+_0x97577c+'_');for(let _0x5d4580 of _0x1f6ee5){await _0x4db153[_0x723d9e(0x15b)](_0x4db153[_0x723d9e(0x146)],_0x5d4580,{},'image');}}catch(_0x172b3d){console[_0x723d9e(0x14a)](_0x172b3d);}}),iris({'name':_0x10aa02(0x122),'fromMe':isPublic,'desc':_0x10aa02(0x134),'category':_0x10aa02(0x13c)},async({m:_0x376842,client:_0x432672,args:_0xa39a0f})=>{const _0x3c0a86=_0x10aa02;_0xa39a0f=_0xa39a0f||_0x376842[_0x3c0a86(0x137)]?.[_0x3c0a86(0x156)];if(!_0xa39a0f)return await _0x376842['reply']('_Enter\x20Link_');var _0x558783=await fetch(_0x3c0a86(0x149)+_0xa39a0f),_0x302112=await _0x558783[_0x3c0a86(0x125)]();_0x432672[_0x3c0a86(0x155)](_0x376842[_0x3c0a86(0x146)],{'document':{'url':_0x302112[_0x3c0a86(0x13f)][_0x3c0a86(0x157)]},'fileName':''+_0x302112['data'][_0x3c0a86(0x152)],'mimetype':_0x3c0a86(0x12b)},{'quoted':_0x376842});}),iris({'name':_0x10aa02(0x12e),'fromMe':isPublic,'desc':_0x10aa02(0x134),'category':_0x10aa02(0x13c)},async({m:_0x3cd1d1,client:_0x522b6e,args:_0x19b9ce})=>{const _0x27a312=_0x10aa02;_0x19b9ce=_0x19b9ce||_0x3cd1d1[_0x27a312(0x137)]?.[_0x27a312(0x156)];if(!_0x19b9ce)return await _0x3cd1d1[_0x27a312(0x128)]('_Enter\x20Link_');var _0x5cd979=await fetch(_0x27a312(0x143)+_0x19b9ce),_0x278f8c=await _0x5cd979[_0x27a312(0x125)]();_0x522b6e['sendMessage'](_0x3cd1d1[_0x27a312(0x146)],{'document':{'url':_0x278f8c[_0x27a312(0x13f)][_0x27a312(0x13e)]},'fileName':''+_0x278f8c['data'][_0x27a312(0x15f)],'mimetype':_0x27a312(0x14d)},{'quoted':_0x3cd1d1});}),iris({'name':_0x10aa02(0x151),'fromMe':isPublic,'desc':_0x10aa02(0x134),'category':_0x10aa02(0x13c)},async({m:_0x5d0a7d,client:_0x11f37a,args:_0x448fb7})=>{const _0x1ece25=_0x10aa02;_0x448fb7=_0x448fb7||_0x5d0a7d[_0x1ece25(0x137)]?.[_0x1ece25(0x156)];if(!_0x448fb7)return await _0x5d0a7d[_0x1ece25(0x128)](_0x1ece25(0x12a));_0x5d0a7d['reply'](_0x1ece25(0x14e));let _0x2c1d95=await fetch(_0x1ece25(0x12d)+_0x448fb7);var _0x1ec470=await _0x2c1d95[_0x1ece25(0x125)]();_0x11f37a[_0x1ece25(0x155)](_0x5d0a7d[_0x1ece25(0x146)],{'video':{'url':_0x1ec470['data']},'caption':_0x1ece25(0x132)},{'quoted':_0x5d0a7d});});
+
+
+const {
+    iris,
+    commands,
+    isPublic
+} = require("../lib/commands.js");
+let gis = require("g-i-s");
+const axios = require('axios');
+const fetch = require('node-fetch');
+
+iris(
+    {
+        name: "insta",
+        fromMe: isPublic,
+        desc: "Instagram downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        let dl = await client.sendMessage(m.jid, {
+            text: "_Downloading..._"
+        }, {
+            quoted: m
+        })
+        try {
+            const res = await axios.get(`https://api.maher-zubair.tech/download/instagram?url=${args}`)
+            let response = await res.data
+            for (let i of response.data) {
+                await m.sendMsg(m.jid, i.url, { quoted : m }, i.type)
+            }
+        } catch (e) {
+            client.sendMessage(m.jid, {
+                text: `_Error_`, edit: dl.key
+            })
+        }
+    }
+);
+
+iris(
+    {
+        name: "story",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+        let dl = await client.sendMessage(m.jid, {
+            text: "_Just a moment..._"
+        }, {
+            quoted: m
+        })
+
+        let url = args
+        let res = await axios.get(`https://api-aswin-sparky.koyeb.app/api/downloader/story?url=${url}`)
+        let response = await res.data
+        let data = response.data[0]
+        let datai = `Total Stories\nUrl : ${url}\n\n`
+        for (let i = 1; i < response.data.length+1; i++) {
+            datai += `_${i} . ${i}/${response.data.length} - ${response.data[i-1].type}_\n`
+        }
+        datai += '\nReply with Number'
+        m.sendMsg(m.jid, datai, {
+            edit: dl.key
+        })
+    }
+);
+
+iris(
+    {
+        name: "image",
+        fromMe: isPublic,
+        desc: "Google Image search",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        try {
+            async function gimage(query, amount = 5) {
+                let list = [];
+                return new Promise((resolve, reject) => {
+                    gis(query, async (error, result) => {
+                        for (
+                            var i = 0;
+                            i < (result.length < amount ? result.length: amount);
+                            i++
+                        ) {
+                            list.push(result[i].url);
+                            resolve(list);
+                        }
+                    });
+                });
+            }
+            if (!args) return await m.reply("_Enter Query,Number_");
+            let [query,
+                amount] = args.split(",");
+            let result = await gimage(query, amount);
+            await m.reply(
+                `_Downloading ${amount || 5} images for ${query}_`
+            );
+            for (let i of result) {
+                await m.sendMsg(m.jid , i, {}, "image")
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+);
+
+
+iris(
+    {
+        name: "gdrive",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+var document= await fetch(`https://api.maher-zubair.tech/download/gdrive?url=${args}`);
+        var zip = await document.json();
+        client.sendMessage(m.jid, { document :{ url: zip.data.downloadUrl }, fileName: `${zip.data.fileName}` , mimetype: "application/x-zip-compressed" }, {quoted: m })
+    }
+    );
+
+iris(
+    {
+        name: "mediafire",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link_");
+var document= await fetch(`https://api.maher-zubair.tech/download/mediafire?url=${args}`);
+        var zip = await document.json();
+        client.sendMessage(m.jid, { document :{ url: zip.data.link }, fileName: `${zip.data.name}` , mimetype: "application/zip" }, {quoted: m })
+    }
+    );
+
+
+iris(
+    {
+        name: "xvdl",
+        fromMe: isPublic,
+        desc: "Instagram story downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+        if (!args) return await m.reply("_Enter Link/Reply to a link_");
+m.reply("_Downloading..._")
+let xvdl = await fetch(`https://api-aswin-sparky.koyeb.app/api/downloader/xdl?url=${args}`);
+var data = await xvdl.json();
+
+  client.sendMessage(m.jid, { video :{ url: data.data }, caption: "ehehehe" }, {quoted: m })
+    }
+  );
+
+
+iris(
+    {
+        name: "ytv",
+        fromMe: isPublic,
+        desc: "yt vid downloader",
+        category: "downloader",
+    },
+    async ({
+        m, client, args
+    }) => {
+        args = args || m.quoted?.text;
+if (!args) return await m.reply("_Reply to a link_");
+let dll = `https://api-viper-x.koyeb.app/api/ytdl?video360p=${args}`
+client.sendMessage(m.jid, { video :{ url: dll }, caption: "_IRIS-MD_"}, {quoted: m })
+    }
+    );
