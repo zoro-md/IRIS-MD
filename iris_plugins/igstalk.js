b2c5e603d1fb7a92deb46e8d03ea5f06c97a138f
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-04-25 23:29:40 +0600

    ɪɢsᴛᴀʟᴋ ᴀᴅᴅᴇᴅ 🍀

---
+++ iris_plugins/igstalk.js
@@ -0,0 +1,22 @@
+const {iris, isPublic} = require("../lib/plugins.js");
+const {INFO} = require("../info.js");
+
+iris(
+    {
+        name: "ig",
+        category: "stalker",
+        fromMe: isPublic,
+        desc: "stalk ur ig account"
+    },
+    async ({
+        client, m, args
+    }) => {
+if (!args) return await m.reply("_Enter Username, account must be public_");
+args = args || m.quoted?.text;
+let sample = await fetch(`https://api-aswin-sparky.koyeb.app/api/search/ig?search=${args}`);
+var data = await sample.json();
+ig = data.data
+let venox = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "displayName": `${INFO.split(";")[0]}`,"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+client.sendMessage(m.jid, { image: { url: ig.avatar }, caption: `_Name : ${ig.name}_\n_Username : ${ig.username}_\n\n_Followers : ${ig.followers}_\n_Following : ${ig.following}_\n_Post : ${ig.posts}_\nBio : ${ig.description}` }, { quoted: venox })
+  }
+);
