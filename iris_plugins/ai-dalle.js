1901ed2cbfa1f1ae8f99d1ca7be7c32a91771f4d
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-04-26 17:40:20 +0600

    Create ai-dalle.js

---
+++ iris_plugins/ai-dalle.js
@@ -0,0 +1,27 @@
+iris(
+    {
+        name: "dalle",
+        fromMe: isPublic,
+        category: "ai",
+        desc: "imagine an image",
+    },
+    async ({
+        m, client, args
+    }) => {
+        if (!args || args.length === 0)
+            return await m.reply("_Please provide a question_");
+      
+        const question = args.join(' ');
+        await m.reply("_Imagining with Dalle..._");
+        const axios = require('axios');
+
+        try {
+            let apiResponse = await axios.get(`https://api.maher-zubair.tech/ai/dalle?q=${encodeURIComponent(question)}`);
+            let imaginedImage = apiResponse.data.imaginedImage;
+            await client.sendMessage(m.jid, { image: imaginedImage }, { quoted: m });
+        } catch (error) {
+            console.error("Error imagining image with Dalle:", error);
+            await m.reply("_Error imagining image with Dalle_");
+        }
+    }
+);
